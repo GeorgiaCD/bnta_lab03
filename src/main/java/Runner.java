2f7116ddad59a2f2c9416b23fccba7700a8bf495
GeorgiaCD <georgiacrawforddellar@gmail.com>
2023-07-12 15:50:39 +0100

    comment

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -9,37 +9,51 @@ public class Runner {
 
 //        SCOTTISH ISLANDS
         List<String> scottishIslands = new ArrayList<>();
-        scottishIslands.add("Tiree");
         scottishIslands.add("Jura");
-        scottishIslands.add("Islay");
         scottishIslands.add("Mull");
         scottishIslands.add("Skye");
         scottishIslands.add("Arran");
         scottishIslands.add("Tresco");
-        scottishIslands.add("Coll");
 
 //        1. Add "Coll" to the end of the list
+        scottishIslands.add("Coll");
+
 //        2. Add "Tiree" to the start of the list
+        scottishIslands.add(0,"Tiree");
+
 //        3. Add "Islay" after "Jura" and before "Mull"
+//      scottishIslands.add(2, "Islay");
+        scottishIslands.add(scottishIslands.indexOf("Jura") +1, "Islay");
+        System.out.println("The first list of Scottish Islands: " + scottishIslands);
+
 //        4. Print out the index position of "Skye"
-        System.out.println(scottishIslands.indexOf("Skye"));
+        int indexOfSkye = scottishIslands.indexOf("Skye");
+        System.out.println("This is the index of Skye: " + indexOfSkye);
+
 //        5. Remove "Tresco" from the list by name
         scottishIslands.remove("Tresco");
+
 //        6. Remove "Arran" from the list by index
         scottishIslands.remove(6);
+
 //        7. Print the number of islands in your arraylist
-        System.out.println("scottishIslands");
+        System.out.println(scottishIslands);
+
 //        8. Sort the list alphabetically
         Collections.sort(scottishIslands);
         System.out.println("Ordered list: " +
                 scottishIslands);
+
+
 //        9. Print out all the islands using a for loop;
         System.out.println(scottishIslands);
         for (int i = 0; i < scottishIslands.size(); i++) {
             System.out.println(scottishIslands.get(i));
         }
         ;
-
+//            for(String islands :scottishIslands){
+//             System.out.println(islands);
+//            }
 
 //        NUMBERS
         List<Integer> numbers = new ArrayList<>();
@@ -48,11 +62,13 @@ public class Runner {
         System.out.println("numbers: " + numbers);
 
 //        1. Print out a list of the even integers
+        List<Integer> evenNumbers = new ArrayList<>();
         for (int number : numbers) {
             if (number % 2 == 0) {
-                System.out.println(number);
+                evenNumbers.add(number);
             }
         }
+        System.out.println("The even numbers are: " + evenNumbers);
 
 //        2. Print the difference between the largest and smallest value
         System.out.println(Collections.max(numbers) - Collections.min(numbers));
@@ -68,9 +84,9 @@ public class Runner {
         boolean sameNumber = false;
         for (int i = 0; i < numbers.size(); i++ ) {
             if (numbers.get(i) == 1 && numbers.get(i + 1) == 1) {
-                System.out.println("True");}
+                sameNumber = true;}
         }
-
+        System.out.println(sameNumber);
 //        4. Print the sum of the numbers,
 
             int sum = 0;
@@ -96,22 +112,35 @@ public class Runner {
 //        int sum2 = 0;
 //        for(int i = 0; i < numbers.size(); i ++) {
 //            sum2 += i;
-//            System.out.println(sum2);
 //            {if(numbers.get(i) == 13){
 //               continue;}
 //            }}
+//        System.out.println(sum2);
 //
 //        System.out.println("The better sum is: " + sum2);
 
 
+//        int sum2 = 0;
+//        for(int number : numbers) {
+//                if(numbers.get(number) == 13){
+//                    break;}
+//            sum2 += number;
+//            }
+//        System.out.println(sum2);
+
+
+
         int sum2 = 0;
         for(int number : numbers) {
-            sum2 += number;
-            System.out.println(sum2);
             {
-                if(numbers.get(number) == 13){
-                break;}
-            }}
+                if (number == 13) {
+                    break;
+                }
+            }
+            sum2 += number;
+        }
+        System.out.println(sum2);
 
     }
 }
+
